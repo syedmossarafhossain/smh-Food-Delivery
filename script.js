d2997e1076fd376de79a8198c7606100e4d5db8f
Syed Mossaraf Hossain <syedmossarafhossain@gmail.com>
2026-07-16 12:51:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ var swiper = new Swiper('.mySwiper', {
         prevEl: '#prev',
     },
 });
+
+
+
+// CART ICON
+
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+
+cartIcon.addEventListener('click', () => cartTab.classList.add('cart-tab-active'));
+closeBtn.addEventListener('click', () => cartTab.classList.remove('cart-tab-active'));
