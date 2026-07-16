4403b143e75b0bd2978a2e1d0b809d67c09cc54d
Syed Mossaraf Hossain <syedmossarafhossain@gmail.com>
2026-07-16 13:18:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,33 @@ var swiper = new Swiper('.mySwiper', {
 const cartIcon = document.querySelector('.cart-icon');
 const cartTab = document.querySelector('.cart-tab');
 const closeBtn = document.querySelector('.close-btn');
+const cardList = document.querySelector('.card-list');
 
 cartIcon.addEventListener('click', () => cartTab.classList.add('cart-tab-active'));
 closeBtn.addEventListener('click', () => cartTab.classList.remove('cart-tab-active'));
+
+
+
+// MENU
+
+let productList = [];
+
+
+const showCards = () => {
+
+    productList.forEach(product => {
+        const orderCard = document.createElement('div');
+        orderCard.classList.add('order-card');
+
+        orderCard.innerHTML = `
+        <div class="card-imag">
+            <img src="${product.image}">
+        </div>
+        <h4>${product.name}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a href="#" class="btn">Add to Cart</a>
+        `;
+
+        cardList.appendChild(orderCard);
+    })
+}
