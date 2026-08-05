18e108b1517b2f52af070c4387152babeb72905b
Syed Mossaraf Hossain <syedmossarafhossain@gmail.com>
2026-08-05 19:38:30 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,45 @@
+// SIGN IN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCJqOyfm6AAPaNQrdaslRQv-hlA4nylaz8",
+    authDomain: "foodie-app-4ade7.firebaseapp.com",
+    projectId: "foodie-app-4ade7",
+    storageBucket: "foodie-app-4ade7.firebasestorage.app",
+    messagingSenderId: "708426307125",
+    appId: "1:708426307125:web:f8ecef9ee597a8c020c37c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const signBtns = document.querySelectorAll(".desktop-action .btn, .mobile-menu .btn");
+
+function getSignButtons() {
+    return [...signBtns];
+}
+
+function updateButtons(user) {
+    signBtns.forEach(btn => {
+        if (user) {
+            btn.innerHTML = `
+                Sign Out &nbsp;
+                <i class="fa-solid fa-right-from-bracket"></i>
+            `;
+        } else {
+            btn.innerHTML = `
+                Sign In &nbsp;
+                <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            `;
+        }
+    });
+}
